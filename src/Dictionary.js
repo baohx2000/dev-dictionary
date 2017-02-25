32cead9e3f18bc236c29fab1bfb251ea320eda8c
Gordon Forsythe <baohx2000@gmail.com>
2017-02-25 11:59:47 -0700

    fetching terms & definitions

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,28 +2,49 @@ import React, { Component } from 'react';
 import { Button, Glyphicon } from 'react-bootstrap';
 import Term from './Term';
 import AddTerm from './AddTerm';
-import jsonData from '../data/db';
-import filter from 'lodash/filter'
-
-// Don't do this. You need to actually fetch the data from the server using
-// the API. This is a cheater way just to provide a visual example of what you
-// should see when you're done.
-const terms = jsonData.terms.map(term => ({
-  ...term,
-  definitions: filter(jsonData.definitions, { termId: term.id })
-}))
-
+import CommonActions from './commonActions';
+import filter from 'lodash/filter';
 
 class Dictionary extends Component {
   state = {
-    showAddTerm: false
+    showAddTerm: false,
+    terms: null,
+    users: null,
   };
 
-  toggleAdd = () => this.setState({ showAddTerm: !this.state.showAddTerm })
+  componentWillMount() {
+    const self = this;
+    CommonActions.fetchJson('/users')
+      .then((data) => setTimeout(function() {self.setState({users:data});}));
+    CommonActions.fetchJson('/terms')
+      .then((data) => setTimeout(function() {self.setState({terms:data});}));
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    const self = this;
+    if (!nextState.definitions && nextState.terms) {
+      CommonActions.fetchJson('/definitions')
+        .then((data) => setTimeout(function() {
+          self.setState({
+            terms:self.state.terms.map((item) =>
+              Object.assign({}, item, {definitions: filter(data, {termId: item.id})})
+            ),
+            definitions: data,
+          });
+        }));
+    }
+  }
+
+  toggleAdd = () => this.setState({ showAddTerm: !this.state.showAddTerm });
 
   render() {
     const { showAddTerm } = this.state;
 
+    if (!this.state.users || !this.state.terms || !this.state.definitions) {
+      return (
+        <div>Initializing</div>
+      );
+    }
     return (
       <div>
         <h2>Terms</h2>
@@ -32,7 +53,7 @@ class Dictionary extends Component {
         </Button>
         {showAddTerm && <AddTerm hide={this.toggleAdd} />}
         <div className="terms">
-          {terms.map(term => {
+          {this.state.terms.map(term => {
             return <Term key={term.id} term={term} />;
           })}
         </div>
--- src/TermContainer.js
+++ src/TermContainer.js
@@ -1,29 +1,111 @@
 import React, { Component } from 'react';
 import { Glyphicon } from 'react-bootstrap';
 import { Link } from 'react-router';
+import CommonActions from './commonActions';
 
-class Term extends Component {
+class TermContainer extends Component {
   static propTypes = {
     params: React.PropTypes.shape({
       termName: React.PropTypes.string.isRequired,
     })
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      termLoaded: false,
+      loaded: false,
+      term: null,
+      definitions: null,
+      error: null,
+    };
+  }
+
+  componentWillMount() {
+    this.setState({loading:true});
+    if (!this.state.loaded) {
+      setTimeout(() => {
+        CommonActions.fetchJson(`/terms?q=${this.props.params.termName}&_limit=1`)
+          .then(
+            (data) => this.setState({
+              term:data[0],
+              termLoaded: true,
+            }),
+            (e) => this.setState({
+              loading: false,
+              error: e,
+            })
+          );
+      });
+    }
+  };
+
+  componentWillUpdate(nextProps, nextState) {
+    if (!this.state.termLoaded && nextState.termLoaded) {
+      if (nextState.term) {
+        setTimeout(() => {
+          CommonActions.fetchJson(`/definitions?termId=${nextState.term.id}`)
+            .then(
+              (data) => this.setState({
+                definitions:data,
+                loaded: true,
+              }),
+              (e) => this.setState({
+                loading: false,
+                error: e,
+              })
+            );
+        });
+      } else {
+        this.setState({
+          loaded: true,
+        });
+      }
+    }
+  }
+
   render() {
+    let content;
+    if (this.state.loaded) {
+      if (!this.state.term) {
+        content = (
+          <div>Term not found</div>
+        );
+      } else {
+        content = (
+          <div>
+            <h1>{this.state.term.name}</h1>
+            <ul>
+              {this.state.definitions.map((item, idx) => (
+                <li key={idx}>{item.content}</li>
+              ))}
+            </ul>
+          </div>
+        );
+      }
+    } else if (this.state.loading) {
+      content = (
+        <div>Loading</div>
+      );
+    } else if (this.state.error) {
+      content = (
+        <div>{this.state.error}</div>
+      );
+    } else {
+      content = (
+        <div>Initializing</div>
+      );
+    }
     return (
       <div className="term">
         <Link to="/terms">
           <Glyphicon glyph="chevron-left" /> Back to terms
         </Link>
-        <h1>[Term name goes here]</h1>
-        <p>Your job here is to fetch the term and show its definitions. Use the <code>{'<Term>'}</code> component.</p>
-        <p>Reminder: to fetch the term from the server by name, you can use the full text search capability with a limit of 1. For example, if the term in the URL is <strong>Full%20Stack</strong>, then you can use the URL</p>
-        <code>
-          /terms?q=Full%20Stack&_limit=1
-        </code>
+        {content}
       </div>
     );
   }
 }
 
-export default Term;
+export default TermContainer;
